4fe7dd12eea158b5d64236982b842895a64c7ee5
JuanPescoran <u20221c936@upc.edu.pe>
2024-05-29 02:55:40 -0500

    chore(counseling): commented part in customized plan

--- src/main/java/com/gymfinity/flexpal/platform/counseling/domain/model/aggregates/CustomizedPlan.java
+++ src/main/java/com/gymfinity/flexpal/platform/counseling/domain/model/aggregates/CustomizedPlan.java
@@ -2,8 +2,10 @@ package com.gymfinity.flexpal.platform.counseling.domain.model.aggregates;
 
 import jakarta.persistence.Entity;
 
+/*
 @Entity
 public class CustomizedPlan {
     public CustomizedPlan() {
     }
 }
+*/
