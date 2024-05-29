b1a087891c9d0114df4cb7c0b915100a837a57a0
JuanPescoran <u20221c936@upc.edu.pe>
2024-05-29 02:54:18 -0500

    feat(counseling): added aggregates in domain layer

---
+++ src/main/java/com/gymfinity/flexpal/platform/counseling/domain/model/aggregates/CustomizedPlan.java
@@ -0,0 +1,9 @@
+package com.gymfinity.flexpal.platform.counseling.domain.model.aggregates;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class CustomizedPlan {
+    public CustomizedPlan() {
+    }
+}
---
+++ src/main/java/com/gymfinity/flexpal/platform/counseling/domain/model/aggregates/Member.java
@@ -0,0 +1,4 @@
+package com.gymfinity.flexpal.platform.counseling.domain.model.aggregates;
+
+public class Member {
+}
