1912ee5ae41b9545859d10d38e3808ea69fafbed
JuanPescoran <u20221c936@upc.edu.pe>
2024-05-29 01:27:45 -0500

    Initial Commit

---
+++ src/main/java/com/gymfinity/flexpal/platform/FlexPalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gymfinity.flexpal.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexPalPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexPalPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gymfinity/flexpal/platform/FlexPalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymfinity.flexpal.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexPalPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
