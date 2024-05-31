d386d4e14b83b5ed36fcdf5d883736da23d4f3c9
angel <angelcca2001@gmail.com>
2024-05-31 05:13:57 -0500

    feat: Added ProfileResource record

---
+++ src/main/java/com/gymfinity/flexpal/platform/profiles/interfaces/rest/resources/ProfileResource.java
@@ -0,0 +1,4 @@
+package com.gymfinity.flexpal.platform.profiles.interfaces.rest.resources;
+
+public record ProfileResource(Long id, String firstName, String lastName, String email, String image) {
+}
