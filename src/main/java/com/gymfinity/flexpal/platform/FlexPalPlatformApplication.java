f58688f63f0455f5e4761a64fd1c14462276aaf0
angel <angelcca2001@gmail.com>
2024-05-31 05:10:20 -0500

    feat: Added SnakeCaseWithPluralizedTablePhysicalNamingStrategy for Hibernate configuration

--- src/main/java/com/gymfinity/flexpal/platform/FlexPalPlatformApplication.java
+++ src/main/java/com/gymfinity/flexpal/platform/FlexPalPlatformApplication.java
@@ -2,8 +2,10 @@ package com.gymfinity.flexpal.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FlexPalPlatformApplication {
 
     public static void main(String[] args) {
